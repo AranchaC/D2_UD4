e6474c4052d4c632943b8c20f305556d1f128bcb
AranchaC <aranchachicharro@gmail.com>
2023-12-18 20:09:56 +0100

    primera subida D2-UD4

---
+++ demo/src/main/java/com/example/demo/Contacto.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+public class Contacto {
+	
+	private String nombre;
+	private String apellido;
+	private int edad;
+	
+	public Contacto(String n, String a, int e) {
+		nombre = n;
+		apellido = a;
+		edad = e;
+	}
+	
+	public Contacto() { }
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String texto) {
+		this.nombre = texto;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String texto) {
+		this.apellido = texto;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+}
